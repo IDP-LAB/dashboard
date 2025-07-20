ed579d531b6a783d41d957e2a5f29fbc41f15716
Ashu <Matheusn.Biolowons@gmail.com>
2025-07-20 09:45:20 -0300

    🔀 Removing merge error

--- server/build/routers.ts
+++ server/build/routers.ts
@@ -1,7 +1,6 @@
 import APIRoot from '../routers/index.js'
 import ConsumeItemByGroup from '../routers/item/consume.js'
 import CreateItem from '../routers/item/create.js'
-import CreateProduct from '../routers/products/create.js'
 import CreateProject from '../routers/project/create.js'
 import CreateUser from '../routers/users/create.js'
 import DeleteItem from '../routers/item/$id/delete.js'
@@ -13,11 +12,9 @@ import EditUser from '../routers/users/$id/edit.js'
 import FindandTransferItem from '../routers/item/transfer.js'
 import GetItem from '../routers/item/$id/get.js'
 import GetProject from '../routers/project/$id/get.js'
-import GetTags from '../routers/products/tags.js'
 import GetUser from '../routers/users/$id/get.js'
 import GetUserProfile from '../routers/users/profile.js'
 import ListItems from '../routers/item/index.js'
-import ListProducts from '../routers/products/index.js'
 import ListProjects from '../routers/project/index.js'
 import ListUsers from '../routers/users/index.js'
 import ReturnItemByGroup from '../routers/item/return.js'
@@ -35,8 +32,6 @@ export const routers = {
   '/item/consume': ConsumeItemByGroup,
   '/item/return': ReturnItemByGroup,
   '/item/transfer': FindandTransferItem,
-  '/products': [CreateProduct, ListProducts],
-  '/products/tags': GetTags,
   '/project': [CreateProject, ListProjects],
   '/project/:id': [DeleteProject, EditProject, GetProject],
   '/users': [CreateUser, ListUsers],
--- server/build/rpc.ts
+++ server/build/rpc.ts
@@ -4,7 +4,6 @@ import type { Router } from '../src/controllers/router.js'
 import type APIRoot from '../routers/index.js'
 import type ConsumeItemByGroup from '../routers/item/consume.js'
 import type CreateItem from '../routers/item/create.js'
-import type CreateProduct from '../routers/products/create.js'
 import type CreateProject from '../routers/project/create.js'
 import type CreateUser from '../routers/users/create.js'
 import type DeleteItem from '../routers/item/$id/delete.js'
@@ -16,11 +15,9 @@ import type EditUser from '../routers/users/$id/edit.js'
 import type FindandTransferItem from '../routers/item/transfer.js'
 import type GetItem from '../routers/item/$id/get.js'
 import type GetProject from '../routers/project/$id/get.js'
-import type GetTags from '../routers/products/tags.js'
 import type GetUser from '../routers/users/$id/get.js'
 import type GetUserProfile from '../routers/users/profile.js'
 import type ListItems from '../routers/item/index.js'
-import type ListProducts from '../routers/products/index.js'
 import type ListProjects from '../routers/project/index.js'
 import type ListUsers from '../routers/users/index.js'
 import type ReturnItemByGroup from '../routers/item/return.js'
@@ -110,25 +107,6 @@ export type Routers = {
       response: MergeUnion<UnwrapPromise<ReturnType<typeof FindandTransferItem.methods.post>>>,
       request: z.infer<NonNullable<typeof FindandTransferItem.schema>['post']>,
       auth: FirstParameter<typeof FindandTransferItem>
-    }
-  },
-  '/products': {
-    get: {
-      response: MergeUnion<UnwrapPromise<ReturnType<typeof ListProducts.methods.get>>>,
-      request: undefined,
-      auth: FirstParameter<typeof ListProducts>
-    },
-    post: {
-      response: MergeUnion<UnwrapPromise<ReturnType<typeof CreateProduct.methods.post>>>,
-      request: z.infer<NonNullable<typeof CreateProduct.schema>['post']>,
-      auth: FirstParameter<typeof CreateProduct>
-    }
-  },
-  '/products/tags': {
-    get: {
-      response: MergeUnion<UnwrapPromise<ReturnType<typeof GetTags.methods.get>>>,
-      request: undefined,
-      auth: FirstParameter<typeof GetTags>
     }
   },
   '/project': {
